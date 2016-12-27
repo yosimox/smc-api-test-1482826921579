1545a4caa253f3b2be963387bc8d78a051ac2894
yosimox <yosimox@gmail.com>
2016-12-27 18:08:26 +0900

    'neko'

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
-# app.py
+"""Cloud Foundry test"""
 from flask import Flask
+import cf_deployment_tracker
+import os
+
+# Emit Bluemix deployment event
+cf_deployment_tracker.track()
+
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+# On Bluemix, get the port number from the environment variable VCAP_APP_PORT
+# When running this app on the local machine, default the port to 8080
+port = int(os.getenv('VCAP_APP_PORT', 8080))
 
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/')
+def hello_world():
+    return 'Hello World! I am running on port ' + str(port)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
