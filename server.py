298b2ee52f5c871de81510ac7a5244cae2f82c37
yosimox <yosimox@gmail.com>
2016-12-27 18:03:33 +0900

    'test'

--- server.py
+++ server.py
@@ -1,16 +1,10 @@
-import os
+# app.py
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/')
-def Welcome():
-    return app.send_static_file('index.html')
+@app.route("/")
+def index():
+    return "Hello World!"
 
-@app.route('/myapp')
-def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
-
-port = os.getenv('PORT', '5000')
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(port))
+    app.run()
